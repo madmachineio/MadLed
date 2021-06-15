9e411d0a81002d4389e2d1841e3f3aff217e477f
Ines Zhou <ines@madmachine.io>
2021-06-15 15:50:14 +0800

    Add comments.

--- Sources/MadLed/MadLed.swift
+++ Sources/MadLed/MadLed.swift
@@ -5,22 +5,23 @@ final public class MadLed {
     
     private let pin: DigitalOut
     private let ledOnValue: Bool
-
+    
+    // Initilize the LED.
     public init(_ pin: DigitalOut, ledOnValue: Bool) {
         self.pin = pin
         self.ledOnValue = ledOnValue
     }
-
+    
     // Turn on the LED.
     public func on() {
         pin.write(ledOnValue)
     }
-
+    
     // Turn off the LED.
     public func off() {
         pin.write(!ledOnValue)
     }
-
+    
     public func toggle() {
         let currentValue = pin.getValue()
         pin.write(!currentValue)
